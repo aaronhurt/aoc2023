cba857bd508b80226c997b517870b0e32a5ef234
Aaron Hurt <aaron.hurt@bayer.com>
2023-12-01 13:51:39 -0600

    misc cleanup

--- day1/main.go
+++ day1/main.go
@@ -1,59 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
+	"maps"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func part1() {
-	file, err := os.Open("./input.txt")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer file.Close()
-
-	reader := bufio.NewScanner(file)
-
-	var total = 0
-
-	for reader.Scan() {
-		line := reader.Text()
-		var numString string
-
-		// get first digit from the start
-		for i := 0; i < len(line); i++ {
-			if _, err := strconv.Atoi(string(line[i])); err == nil {
-				numString += string(line[i])
-				break
-			}
-		}
-
-		// get first digit from the end
-		for i := len(line) - 1; i >= 0; i-- {
-			if _, err := strconv.Atoi(string(line[i])); err == nil {
-				numString += string(line[i])
-				break
-			}
-		}
-
-		// check the string and sum total
-		if num, err := strconv.Atoi(numString); err == nil {
-			total += num
-		} else {
-			log.Fatal(err)
-		}
-	}
-
-	fmt.Printf("P1 Total: %d\n", total)
+var digitMap = map[string]int{
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
 }
-
-var digitWords = map[string]int{
+var wordMap = map[string]int{
 	"one":   1,
 	"two":   2,
 	"three": 3,
@@ -65,51 +32,86 @@ var digitWords = map[string]int{
 	"nine":  9,
 }
 
-func isDigit(line string, idx int) (bool, string) {
-	if _, err := strconv.Atoi(string(line[idx])); err == nil {
-		return true, string(line[idx])
-	}
-	for word, digit := range digitWords {
-		if strings.HasPrefix(line[idx:], word) {
-			return true, strconv.Itoa(digit)
+func isDigit(line string, idx int, m map[string]int) (bool, int) {
+	for str, digit := range m {
+		if strings.HasPrefix(line[idx:], str) {
+			return true, digit
 		}
 	}
-	return false, ""
+	return false, 0
 }
-func part2() {
-	file, err := os.Open("./input.txt")
 
-	if err != nil {
+func readLines() []string {
+	var data []byte
+	var err error
+
+	if data, err = os.ReadFile("./input.txt"); err != nil {
 		log.Fatal(err)
 	}
 
-	defer file.Close()
+	return strings.Split(string(data), "\n")
+}
 
-	reader := bufio.NewScanner(file)
-
+func part1() {
 	var total = 0
 
-	for reader.Scan() {
-		line := reader.Text()
+	for _, line := range readLines() {
 		var numString string
+		var length = len(line)
 
 		// get first digit from the start
-		for i := 0; i < len(line); i++ {
-			if ok, str := isDigit(line, i); ok {
-				numString += str
+		for i := 0; i < length; i++ {
+			if ok, digit := isDigit(line, i, digitMap); ok {
+				numString += strconv.Itoa(digit)
 				break
 			}
 		}
 
 		// get first digit from the end
-		for i := len(line) - 1; i >= 0; i-- {
-			if ok, str := isDigit(line, i); ok {
-				numString += str
+		for i := length - 1; i >= 0; i-- {
+			if ok, digit := isDigit(line, i, digitMap); ok {
+				numString += strconv.Itoa(digit)
 				break
 			}
 		}
 
-		// check the string and sum total
+		// check the concatenated string and sum total
+		if num, err := strconv.Atoi(numString); err == nil {
+			total += num
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Printf("P1 Total: %d\n", total)
+}
+
+func part2() {
+	var total = 0
+	var fullMap = maps.Clone(digitMap)
+	maps.Copy(fullMap, wordMap)
+
+	for _, line := range readLines() {
+		var numString string
+		var length = len(line)
+
+		// get first digit from the start
+		for i := 0; i < length; i++ {
+			if ok, digit := isDigit(line, i, fullMap); ok {
+				numString += strconv.Itoa(digit)
+				break
+			}
+		}
+
+		// get first digit from the end
+		for i := length - 1; i >= 0; i-- {
+			if ok, digit := isDigit(line, i, fullMap); ok {
+				numString += strconv.Itoa(digit)
+				break
+			}
+		}
+
+		// check the concatenated string and sum total
 		if num, err := strconv.Atoi(numString); err == nil {
 			total += num
 		} else {
